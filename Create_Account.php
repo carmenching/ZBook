d18c1b723c3914fc5cb243ef33f6549d45498ae6
DRIVET Cécile <drivet.cecile@gmail.com>
2019-05-13 12:08:16 +0200

    ADD php part for user creation

---
+++ Create_Account.php
@@ -0,0 +1,14 @@
+<?php
+//Connexion à la BDD
+
+
+//Ajout d'un utilisateur
+$Lastname
+$Firstname
+$Pseudo
+$Password
+$Email
+$Birthdate
+
+"INSERT INTO USER (LastNameUser, FirstNameUser, PseudoUser, PasswordUser, MailUser, BirthDateUser) VALUES ($Lastname, $Firstname, $Pseudo, $ Password, $Email, $Birthdate);"
+?>
