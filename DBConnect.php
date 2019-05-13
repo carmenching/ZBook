684894a42431d2d2c2782c12c6662dea60ab7302
K1nkku <k1nkku.ciabrini@gmail.com>
2019-05-13 11:13:50 +0200

    Delete DBConnect.php

--- DBConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$DB['server'] = '185.98.131.90';
-$DB['user'] = 'breto1088830_2spl7';
-$DB['password'] = '7yng5velfj';
-$DB['db'] = 'breto1088830_2spl7';
-$DB['prefix'] = '';
-$DB['type'] = 'mysqli';
-?>
