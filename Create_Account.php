ef82469541e7010a63b563e561f2b8f08709a8d8
DRIVET Cécile <drivet.cecile@gmail.com>
2019-05-14 08:56:51 +0200

    ADD SQL request to create a new user

--- Create_Account.php
+++ Create_Account.php
@@ -1,14 +1,30 @@
 <?php
 //Connexion à la BDD
+//Récupérer les données de connexions
+$servername = '185.98.131.90';
+$username = 'breto1088830_2spl7';
+$password = '7yng5velfj';
+$db = 'breto1088830_2spl7';
 
+// Connexion à la DB
+$conn = new mysqli($servername, $db, $username, $password);
+
+//Checker que la connexion fonctionne
+if ($cnx->cnx_error){
+	die("Impossible de se connecter" . $connection->connect_error);
+}
+echo "Connexion réussie !";
 
 //Ajout d'un utilisateur
-$Lastname
-$Firstname
-$Pseudo
-$Password
-$Email
-$Birthdate
+$Lastname;
+$Firstname;
+$Pseudo;
+$Password;
+$Email;
+$Birthdate;
 
-"INSERT INTO USER (LastNameUser, FirstNameUser, PseudoUser, PasswordUser, MailUser, BirthDateUser) VALUES ($Lastname, $Firstname, $Pseudo, $Password, $Email, $Birthdate);"
+
+
+$res = $mysqli->query("INSERT INTO USER (LastNameUser, FirstNameUser, PseudoUser, PasswordUser, MailUser, BirthDateUser) VALUES ($Lastname, $Firstname, $Pseudo, $ Password, $Email, $Birthdate)";
+
 ?>
