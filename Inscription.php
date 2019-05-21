36176e74a667da42a3d9cd8ca5bce4e48f965ca2
chany <chaniliaroyal@gmail.com>
2019-05-21 09:02:22 +0200

    added connection and inscription page with stylesheet

---
+++ Inscription.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta  charset="utf-8"/>
+ <title>Inscription</title>
+ <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+   <div class="signup">
+    <form>
+     <h2 style="color: #fff;">Créer un compte</h2>
+     <input type="pseudo" name="pseudonyme" placeholder="Pseudo"><br><br>
+     <input type="text" name="username" placeholder="Prenom"><br><br>
+     <input type="text" name="username" placeholder="Nom"><br><br>
+     <input type="password" name="pass" placeholder="Mot de passe"><br><br>    
+     <input type="password" name="pass" placeholder="Confirmez mot de passe"><br><br>   
+     <input type="text" name="mail" placeholder="Adresse mail"><br><br>
+     <input type="date" name="date" placeholder="Date de naissance"><br><br>
+     <input type="button" value="Inscription" onclick="myFunction()"><br><br>
+    </form>
+ </div>
+</body>
+</html>
+
---
+++ connec.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+  <html>
+ <head>
+<meta charset="utf-8" />
+<title>Connexion</title>
+<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+   <div class="signin">
+<form>
+ <h2 style="color:#fff;">Connexion</h2>
+   <input type="text" name="username" placeholder="Nom d'utilisateur"/><br /><br />
+   <input type="password" name="password" placeholder="Mot de passe" /><br /><br />
+   <a href=".html"><input type="button" value="Connexion" /></a><br /><br />
+       <div id="container">
+         Pas encore de compte? <a href="Inscription.html" style="font-family:'Play', sans-serif;">Inscrivez-vous ici</a>
+         </form>
+      </div>
+    </body>
+ </html>
+
+
