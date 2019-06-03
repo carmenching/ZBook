44d70bd56647a3c3e41046e95b39c4f21f12484f
bobtoutpuissant <odelalande@gmail.com>
2019-06-03 09:28:30 +0200

    don't remember

--- index.php
+++ index.php
@@ -16,11 +16,20 @@ require('headerpreset.html');
 	<div id="MIDDLE" class="col-12 col-sm-10 col-md-8">
 
 		<!-- module de publication -->
-		<div id="PUBLISHER">
-			<form action="publications.php" method="post">
-			<p>Publier :</p>
-			<input type="text" name="postContent">
-			<button type="submit">Submit</button>
+		<div id="PUBLISHER" class="row">
+			<form action="publications.php" method="post" class="col-12">
+				<div class="lilspacer row"></div>
+				<div class="row">
+					<div class="col-2"><img src=""></div>
+					<p class="col-8 text-center bt" id="POSTTOPTEXT">Publier Un Post</p>
+					<div class="col-2"></div>
+				</div>
+				<div class="row">
+					<img src="" class="col-1">
+					<textarea class="form-control col-10" rows="4"></textarea>
+					<div class="col-1"><div class="row"><input type="image" class="col-12" src="img/buttonmessage.png" id="BUTTONPUB" style="width: 40px; height: 40px;"></input></div></div>
+				</div>
+				<div class="lilspacer row"></div>
 			</form>
 		</div>
 		
@@ -38,6 +47,26 @@ require('headerpreset.html');
 		}
 		$mysqli->close();
 ?>
+            <article class="fullpost">
+                <p><a href="">Robert Roger</a></p>
+                <p class="postcontent">J'ai mangé un ananas ce midi, c'était bon putain! j'aime me mettre des concombres dans les oreilles en pensant à ta mère. Je crois bien que je vis ma meilleure vie. #CACTUS</p>
+            </article>
+
+            <article class="fullpost">
+                <p><a href="">Robert Roger</a></p>
+                <p class="postcontent">J'ai mangé un ananas ce midi, c'était bon putain! j'aime me mettre des concombres dans les oreilles en pensant à ta mère. Je crois bien que je vis ma meilleure vie. #CACTUS</p>
+            </article>
+
+            <article class="fullpost">
+                <p><a href="">Robert Roger</a></p>
+                <p class="postcontent">J'ai mangé un ananas ce midi, c'était bon putain! j'aime me mettre des concombres dans les oreilles en pensant à ta mère. Je crois bien que je vis ma meilleure vie. #CACTUS</p>
+            </article>
+
+            <article class="fullpost">
+                <p><a href="">Robert Roger</a></p>
+                <p class="postcontent">J'ai mangé un ananas ce midi, c'était bon putain! j'aime me mettre des concombres dans les oreilles en pensant à ta mère. Je crois bien que je vis ma meilleure vie. #CACTUS</p>
+            </article>
+
             <article class="fullpost">
                 <p><a href="">Robert Roger</a></p>
                 <p class="postcontent">J'ai mangé un ananas ce midi, c'était bon putain! j'aime me mettre des concombres dans les oreilles en pensant à ta mère. Je crois bien que je vis ma meilleure vie. #CACTUS</p>
