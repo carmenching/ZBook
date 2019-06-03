f109cff2a0a6e5e2e587e5dc5f22ddc8b03a9bf6
Guillaume <guillaumelecaiyou@gmail.com>
2019-06-03 10:36:46 +0200

    olive gogogo

--- main.js
+++ main.js
@@ -1,38 +1,16 @@
-$('#envoi').click(function(e){
-  e.preventDefault(); 
-
-  var pseudo = encodeURIComponent( $('#pseudo').val() ); // sécurisation
-  var message = encodeURIComponent( $('#message').val() );
-
-  if(pseudo != "" && message != ""){ 
-      $.ajax({
-          url : "messagerie.php", // on donne l'URL du fichier de traitement
-          type : "POST", // la requête est de type POST
-          data : "pseudo=" + pseudo + "&message=" + message // et on envoie nos données
-      });
-
-     $('#messages').append("<p>" + pseudo + " dit : " + message + "</p>"); 
-  }
-});
-function charger(){
-
-  setTimeout( function(){
-
-      var premierID = $('#messages p:first').attr('id'); 
-
-      $.ajax({
-          url : "charger.php?id=" + premierID, 
-          type : GET,
-          success : function(html){ //envoi du message 
-              $('#messages').prepend(html);
-          }
-      });
-
-      charger();
-
-  }, 5000);
-
-}
-
-charger();
-
+    $(function() {
+        $('#messagerie form').submit(function() {
+            var auteur = $('#messagerie #auteur').val();
+            var corps = $('#messagerie #corps').val();
+            $.ajax({
+                type: "POST",
+                url: "Testajax.php",
+                data: 'auteur=' + auteur + '&corps=' + corps,
+                success: function(data) {
+                    $('#messages').prepend(data);
+                    $('#nopost').hide();
+                }
+            });
+            return false;
+        });
+    });
