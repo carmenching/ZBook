9780cab984da29302511b03235469075074e35fe
Carmen Ching <carmen.ching@esicad.org>
2019-06-03 12:26:40 +0200

    modified index.php with publications branch

--- index.php
+++ index.php
@@ -1,48 +1,71 @@
 <?php
 require('config/DBCNX.php');
-include 'template/header.html';
 
+// recuperer la session d'utilisateur et injecter l'info sur header
+session_start();
+include 'template/header.php';
+require('headerpreset.html');
+
+if(empty($_SESSION['username'])) {
+	header('Location: login.php');
+} 
 ?>
 
 <!-- contient la page en elle même, timeline + barre latérale, .... -->
-<div id="FULLPAGE">	
+<div class="spacer"></div>
+
+<div id="FULLPAGE" class="row">	
 
 	<!-- Barre latérale gauche -->
-	<div class="sidebar">
-		<ul id="LEFTBAR">
-			<li class="sidebarlink"><a href="">PROFIL</a></li>
-			<li class="sidebarlink"><a href="">CHAT</a></li>
-			<li class="sidebarlink"><a href="">AMIS</a></li>
-		</ul>
+	<div class="void col-0 col-sm-1 col-md-2">
+
 	</div>
 
 	<!-- contient la division centrale de la page (timeline + module de publication) -->
-	<div id="MIDDLE">
+	<div id="MIDDLE" class="col-12 col-sm-10 col-md-8">
 
 		<!-- module de publication -->
-		<div id="PUBLISHER">
-			<form id="submit_post">
-			<p>Publier :</p>
-			<input type="text" name="postContent" id="postContent">
-			<input type="submit" value="Send">
+<div id="PUBLISHER" class="row">
+<form action="publications.php" method="post" class="col-12">
+	<div class="lilspacer row"></div>
+	<div class="row">
+		<div class="col-2"><img src=""></div>
+		<p class="col-8 text-center bt" id="POSTTOPTEXT">Publier Un Post</p>
+		<div class="col-2"></div>
+	</div>
+	<div class="row">
+		<img src="" class="col-1">
+		<input type='text' class="form-control col-10" rows="4" name="postContent" id="postContent"></input>
+		<div class="col-1"><div class="row"><input type="submit" valeur="Send" class="col-12" src="img/buttonmessage.png" id="BUTTONPUB" style="width: 40px; height: 40px;"></input></div></div>
+	</div>
+	<div class="lilspacer row"></div>
 			</form>
 		</div>
 		
 		<!-- la timeline en elle même -->
 		<div id="TIMELINE">
-
 			<!-- ceci est un type post: -->
 
 <?php
 		// recuperer tous les posts depuis la bdd
-		if ($fetch = $mysqli->query("SELECT DatePost, ContentPost FROM POST ORDER BY DatePost DESC")) {
-			while ($post = $fetch->fetch_assoc()) {
-				echo "<article class=\"fullpost\">
-				<p><a href=\"\">Robert Roger</a><small>".$post['DatePost']."</small></p>
-				<p class=\"postcontent\">" . $post['ContentPost'] . "</p></article>";
-			}
-		}
-		$mysqli->close();
+		$querySearch = "SELECT post.IDPost, user.FirstNameUser, user.LastNameUser, post.DatePost, post.ContentPost, COUNT(post_like.IDPostLike) AS NumberLikes 
+                    FROM post LEFT JOIN post_like ON post_like.IDPost = post.IDPost, user 
+                    WHERE post.IDUser = user.IDUser 
+                    GROUP BY IDPost 
+                    ORDER BY post.DatePost DESC";
+                    
+    if ($fetch = $mysqli->query($querySearch)) {
+        while ($post = $fetch->fetch_assoc()) {
+            echo 
+            "<article class=\"fullpost\">
+            <p><a href=\"\">".$post['FirstNameUser']." ".$post['LastNameUser']."</a><small>".$post['DatePost']."</small></p>
+            <p class=\"postcontent\">" . $post['ContentPost'] . "</p>
+            <a class=\"likePost\" href=\"publications_like.php/?like=". $post['IDPost']."\"><img src=\"img/like.png\" alt=\"like icon\" style=\"width:20px\"></a>
+            <p id =\"postid".$post['IDPost']. "\" class=\"likeCount\">".intval($post['NumberLikes'])."</p>
+            </article>";
+        }
+    }
+    $mysqli->close();
 ?>
 		
 			<!-- <article class="fullpost">
@@ -55,15 +78,15 @@ include 'template/header.html';
 
 
 	<!-- Barre latérale de menu -->
-	<div class="sidebar">
-		<ul id="RIGHTBAR">
-			<li class="sidebarlink"><a href="">PROFIL</a></li>
-			<li class="sidebarlink"><a href="">CHAT</a></li>
-			<li class="sidebarlink"><a href="">AMIS</a></li>
-		</ul>
+	<div class="void col-0 col-sm-1 col-md-2">
 	</div>
 </div>
 
 <script src="ajax/publications.js"></script>
+<div class="spacer"></div>
+
+<?php
+require('footerpreset.html');
+?>
 </body>
 </html>
