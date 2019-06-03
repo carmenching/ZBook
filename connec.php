838c6888f39e7adc64edaa5832372291998a4477
Solal <Bretonsolal65@gmail.com>
2019-06-03 12:00:23 +0200

    COde fonctionnel script connexion à améliorer

---
+++ connec.php
@@ -0,0 +1,20 @@
+<?php
+
+?> 
+
+<form action="connect.php" method='post'>
+          <table align="left" border="0" class="text1">
+            <tr>
+    <td>Login :</td>
+    <td><input type="text" name="PseudoUser"></td>
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td><input type="password" name="PasswordUser"></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center"><input type="submit" value="Connexion"></td>
+  </tr>
+</table>
+</form></p>
+<?php?>
