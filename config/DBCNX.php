f6ee5c0e97f36397078051af320e17bf75a37c7f
Carmen <carmen.karyee@outlook.com>
2019-06-16 01:45:49 +0200

    configured DB file

--- config/DBCNX.php
+++ config/DBCNX.php
@@ -2,16 +2,16 @@
 
 //Récupérer les données de connexions
 // <--------serveur scallingo---------->
-// $servername = '9883e481-c82d-46c5-b38a-fc00215f3a9c.zbooklike-4405.mysql.dbs.scalingo.com:32235';
-// $username = 'zbooklike_4405';
-// $password = 'bzDPCikzVTgmw1hmv1Tn';
-// $db = 'zbooklike_4405';
+$servername = '9883e481-c82d-46c5-b38a-fc00215f3a9c.zbooklike-4405.mysql.dbs.scalingo.com:32235';
+$username = 'zbooklike_4405';
+$password = 'bzDPCikzVTgmw1hmv1Tn';
+$db = 'zbooklike_4405';
 
 // <--------local server---------->
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$db = 'zbook';
+// $servername = 'localhost';
+// $username = 'root';
+// $password = '';
+// $db = 'zbook';
 $rootPath = "http://".$_SERVER['SERVER_NAME']."/zbook/";
 $home = $rootPath."index.php";
 
